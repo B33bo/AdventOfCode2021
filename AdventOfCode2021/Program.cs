b09e5f1d545b48cda688a555598a18f208ab414c
Brandon Piper <brandonpiper1@gmail.com>
2021-12-06 17:55:46 +0000

    fixed program.cs

--- AdventOfCode2021/Program.cs
+++ AdventOfCode2021/Program.cs
@@ -8,19 +8,11 @@ namespace AdventOfCode2021
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine(Day5.SolveA());
             Console.WriteLine(Day6.SolveA());
-            /*Vector2.Intersections(new(1, 1), new(7, 7), true);
-            Console.WriteLine();
 
-            Vector2.Intersections(new(7, 7), new(1, 1), true);
-            Console.WriteLine();
+            Console.WriteLine("-----");
 
-            Vector2.Intersections(new(8, 3), new(10, 1), true);
-            Console.WriteLine();
-
-            Vector2.Intersections(new(10, 1), new(8, 3), true);
-            Console.WriteLine();*/
+            Console.WriteLine(Day6.SolveB());
         }
     }
 }
